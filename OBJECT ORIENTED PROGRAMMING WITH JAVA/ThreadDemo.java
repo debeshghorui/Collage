ee40959d16b223c39eab604188a8ea5dddf33a6e
Debesh <debeshghorui@gmail.com>
2025-07-25 20:38:11 +0530

    Add ThreadDemo class to demonstrate the use of threads in Java

---
+++ OBJECT ORIENTED PROGRAMMING WITH JAVA/ThreadDemo.java
@@ -0,0 +1,40 @@
+// Java program to demonstrate the use of threads
+
+class MyThread extends Thread {
+    private String threadName;
+
+    MyThread(String name) {
+        this.threadName = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + " - Count: " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                System.out.println(threadName + " interrupted.");
+            }
+        }
+        System.out.println(threadName + " finished.");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread 1");
+        MyThread t2 = new MyThread("Thread 2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+
+        System.out.println("Main thread finished.");
+    }
+}
