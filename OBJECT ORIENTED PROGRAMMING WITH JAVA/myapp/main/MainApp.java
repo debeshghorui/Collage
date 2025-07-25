14c92576f929201b36d5b2f0bfcc1dd981bb7dad
Debesh <debeshghorui@gmail.com>
2025-07-25 20:27:25 +0530

    MainApp and Helper classes to implement message printing functionality

--- OBJECT ORIENTED PROGRAMMING WITH JAVA/myapp/main/MainApp.java
+++ OBJECT ORIENTED PROGRAMMING WITH JAVA/myapp/main/MainApp.java
@@ -1,5 +1,10 @@
 package myapp.main;
 
+import myapp.utils.Helper;
+
 public class MainApp {
-    
+    public static void main(String[] args) {
+        System.out.println("MainApp running...");
+        Helper.printMessage("Hello from sub-package!");
+    }
 }
--- OBJECT ORIENTED PROGRAMMING WITH JAVA/myapp/utils/Helper.java
+++ OBJECT ORIENTED PROGRAMMING WITH JAVA/myapp/utils/Helper.java
@@ -1,5 +1,7 @@
 package myapp.utils;
 
 public class Helper {
-    
+    public static void printMessage(String msg) {
+        System.out.println("Helper says: " + msg);
+    }
 }
